fcd11447595afe4d3a08c3b910313f7f34c11efc
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-04-29 18:42:50 -0700

    Add bubble sort unit test

--- test/bubble_sort_test.js
+++ test/bubble_sort_test.js
@@ -1,2 +1,11 @@
 const assert = require('chai').assert
 const bubbleSort = require('../lib/bubble_sort')
+
+describe('bubble sort', function() {
+  it('sorts elements of an array by bubble-sort algorithm', function() {
+    var unsorted = [2, 0, 1, 3, 4, 5];
+    var expected = [0, 1, 2, 3, 4, 5];
+    var result = bubbleSort(unsorted);
+    assert.equal(result, expected);
+  });
+});
